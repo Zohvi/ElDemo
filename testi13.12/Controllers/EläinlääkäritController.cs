aa33587beb3ad3241a6315f46b1173f6920b16b5
SoileN <inkinyman@gmail.com>
2018-12-19 15:58:51 +0200

    Luotu eläinlääkärit controller ja view.

---
+++ testi13.12/Controllers/EläinlääkäritController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using testi13._12.Models;
+
+namespace testi13._12.Controllers
+{
+    public class EläinlääkäritController : Controller
+    {
+        public IActionResult Index()
+        {
+            EläimiäContext context = new EläimiäContext();
+            List<Eläinlääkärit> eläinlääkäri = context.Eläinlääkärit.ToList();
+            return View(eläinlääkäri);
+        }
+        public IActionResult UusiEläinlääkäri()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult UusiEläinlääkäri(Eläinlääkärit uusi)
+        {
+            EläimiäContext context = new EläimiäContext();
+            context.Eläinlääkärit.Add(uusi);
+            context.SaveChanges();
+            return View();
+
+        }
+
+    }
+
+
+}
